47506577d1705b79ed530379d9a40a5f92e12505
aspen <jgromadzinska9@gmail.com>
2023-07-11 22:59:35 +0100

    Loading assets test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Is this working? Oh, yes.")
+import './style/reset.css';
+import './style/style.css';
+import testImage from './assets/sophia-unsplash.jpg';
+
+console.log("Is this working? Oh, yes.");
+
+//testing image loading
+const testDiv = document.createElement('div');
+testDiv.classList.add('test-div');
+testDiv.textContent = "PenguinBistre";
+
+const glacierImg = new Image();
+glacierImg.src = testImage;
+
+testDiv.appendChild(glacierImg);
+
+document.body.appendChild(testDiv);
