32fc5a6952c30902c958d5df13fd021028909458
aspen <jgromadzinska9@gmail.com>
2023-07-13 19:24:40 +0100

    Hardcode base HTML layout

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# odin-restaurant-page
+# odin-restaurant-page  
+This is a project from the [Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page).
+<br>
+<br>
+The aim of this project is to practice using webpack and modules by dynamically rendering a simple restaurant homepage.
+<br>
+<br>
+#TO-DO  
+-remove hardcoded HTML design and render it using Javascript  
+-use modules to set up tabbed browsing
--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import testImage from './assets/sophia-unsplash.jpg';
 console.log("Is this working? Oh, yes.");
 
 //testing image loading
-const testDiv = document.createElement('div');
-testDiv.classList.add('test-div');
-testDiv.textContent = "PenguinBistre";
+//const testDiv = document.createElement('div');
+//testDiv.classList.add('test-div');
+//testDiv.textContent = "PenguinBistre";
 
-const glacierImg = new Image();
-glacierImg.src = testImage;
+//const glacierImg = new Image();
+//glacierImg.src = testImage;
 
-testDiv.appendChild(glacierImg);
+//testDiv.appendChild(glacierImg);
 
-document.body.appendChild(testDiv);
+//document.body.appendChild(testDiv);
